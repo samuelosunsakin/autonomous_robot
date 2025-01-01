7d84ded74bc842bdeffaef5af6418009b92d08c0
Samuel Osunsakin <samstarrysam@gmail.com>
2025-01-01 12:56:08 +0000

    Learning to use i2c devices

--- Step_by_step_Building/Communication_with_sensors/hello_i2c_lsm303.cpp
+++ Step_by_step_Building/Communication_with_sensors/hello_i2c_lsm303.cpp
@@ -1,8 +1,6 @@
 /*
 hello_i2c_lsm303.cpp is a program to learn to use I2C devices
 with the PIGPIO library for the Raspberry Pi.
-This program is an accompaniment to the book Practical Robotics in C++
-written by Lloyd Brombach and published by Packt Publishing
 */
 
 #include <iostream>
